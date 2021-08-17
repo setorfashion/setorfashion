f614dad13aa8cdb87e8fdda91330b4304580b7fd
setorfashion <setorfashionprojeto@gmail.com>
2021-08-17 16:27:22 -0300

    change_10.0.0.27

--- server/src/controllers/tokenController.js
+++ server/src/controllers/tokenController.js
@@ -7,30 +7,17 @@ const { get } = require("axios").default;
 const { post } = require("request");
 const { promisify } = require("util");
 require("dotenv").config();
+const http = require('http')
 
 const postAsync = promisify(post);
 
 
 module.exports = {
-    // async receiveToken(req,res){
-    //     const user = req.user
-    //     const shortToken = req.params.shortToken
-    //     const insert = {
-    //         userId: user._id,
-    //         shortToken
-    //     }
-    //     Token.create(insert).then(createdToken=>{
-    //         return res.status(201).json(createdToken)
-    //     }).catch(err=>{
-    //         return res.status(402).json({msg: JSON.stringify(err)})
-    //     })
-        
-    // },
-    async receiveToken(req,res) {
-        // sending the request.
-        const shortToken = req.body.shortToken
+    async receiveToken(req,res){
         const user = req.user
-        postAsync({
+        const shortToken = req.params.shortToken
+        console.log('shortToken: '+shortToken)
+        const options ={
           url: `https://api.instagram.com/oauth/access_token `,
           formData: {
             client_id: 261340495802382,
@@ -43,36 +30,65 @@ module.exports = {
             "content-type": "multipart/form-data",
             host: "api.instagram.com",
           },
-        }).then((resp)=>{
-          console.log('resposta: '+resp)
-        }).catch(err=>{
-          console.log('erro da consulta do token'+JSON.stringify(err))
+        }
+        var reqst = http.request(options, function(res) {
+          console.log('STATUS: ' + res.statusCode);
+          console.log('HEADERS: ' + JSON.stringify(res.headers));
+          res.setEncoding('utf8');
+          res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+          });
         });
-        // console.log('retorno Insta: '+ JSON.stringify(retornoInsta))
-        // const body = retornoInsta.body
-        // const statusCode = retornoInsta.statusCode
+        reqst.on('error', function(e) {
+          console.log('problem with request: ' + e.message);
+        });
+        reqst.end();
+
+        
+    },
+    async receiveToken_(req,res) {
+        // sending the request.
+        const shortToken = req.body.shortToken
+        const user = req.user
+        console.log('shortToken: '+shortToken)
+        let { body, statusCode } = await postAsync({
+          url: `https://api.instagram.com/oauth/access_token `,
+          formData: {
+            client_id: 261340495802382,
+            client_secret: '76eb60ab926342457302b9441cc38ebd',
+            redirect_uri: 'https://sf.fortaldelivery.com.br/token',
+            code: shortToken,
+            grant_type: "authorization_code",
+          },
+          headers: {
+            "content-type": "multipart/form-data",
+            host: "api.instagram.com",
+          },
+        }).catch(err=>{
+          console.log('erro da consulta do token '+JSON.stringify(err))
+        });
       
-        // let response = JSON.parse(body);
-        // console.log('resposta '+JSON.stringify(response))
-        // console.log('statusCode '+statusCode)
-        // if (statusCode !== 200) {
-        //   let error_message = response.error_message;
-        //   return res.status(402).json({msg:error_message})
-        // }
+        let response = JSON.parse(body);
+        console.log('resposta '+JSON.stringify(response))
+        console.log('statusCode '+statusCode)
+        if (statusCode !== 200) {
+          let error_message = response.error_message;
+          return res.status(402).json({msg:error_message})
+        }
        
-        // const insert = {
-        //   shortToken,
-        //   longToken: response.access_token,
-        //   userId: user._id
-        // }
-        // console.log('inserir no banco '+JSON.stringify(insert))
-        // Token.save(insert).then((tokensaved)=>{
-        //   console.log('retorno '+JSON.stringify(tokensaved))
-        //     return res.status(201).json(tokensaved)
-        // }).catch (err=>{
-        //   console.log('erro do insert'+JSON.stringify(tokensaved))
-        //     return res.status(402).json({erro:err})
-        // })
+        const insert = {
+          shortToken,
+          longToken: response.access_token,
+          userId: user._id
+        }
+        console.log('inserir no banco '+JSON.stringify(insert))
+        Token.save(insert).then((tokensaved)=>{
+          console.log('retorno '+JSON.stringify(tokensaved))
+            return res.status(201).json(tokensaved)
+        }).catch (err=>{
+          console.log('erro do insert'+JSON.stringify(tokensaved))
+            return res.status(402).json({erro:err})
+        })
         // return res.status(201).json(response.access_token)
       }
 
