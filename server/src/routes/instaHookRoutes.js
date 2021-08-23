916be51a037a307236a3a2868f2c9c50f7794f2d
setorfashion <setorfashionprojeto@gmail.com>
2021-08-23 12:53:43 -0300

    change_1.0.1.7

--- server/src/routes/instaHookRoutes.js
+++ server/src/routes/instaHookRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = express.Router()
 const instaHooks = require('../controllers/instaHooksController')
 
-routes.get('/gethook/:key',instaHooks.txHooks)
-routes.post('/gethook/:key',instaHooks.txHooks)
+routes.get('/gethook/:key',instaHooks.gettxHooks)
+routes.post('/gethook/:key',instaHooks.posttxHooks)
 
 module.exports = routes 
