d3faa03f8eb53d870c92e4eed9c012d91649bf0b
setorfashion <setorfashionprojeto@gmail.com>
2021-08-23 13:15:44 -0300

    change_1.0.1.17

--- server/src/controllers/instaHooksController.js
+++ server/src/controllers/instaHooksController.js
@@ -3,7 +3,7 @@ const { json } = require('express')
 module.exports={
     // 23dffa$##125Poams897
     async gettxHooks(req,res){
-        var key = JSON.parse(req.query)
+        var key = JSON.parse(JSON.stringify(req.query))
         console.log(key)
         console.log(key['challenge'])
         return res.status(201).json(key)
