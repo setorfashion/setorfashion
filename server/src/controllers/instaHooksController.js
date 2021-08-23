98155ec9e2149c6dc46d689e360d8c5051065287
setorfashion <setorfashionprojeto@gmail.com>
2021-08-23 13:02:29 -0300

    change_1.0.1.8

--- server/src/controllers/instaHooksController.js
+++ server/src/controllers/instaHooksController.js
@@ -3,13 +3,13 @@ const { json } = require('express')
 module.exports={
     // 23dffa$##125Poams897
     async gettxHooks(req,res){
-        const key = req.params.key
-        console.timeLog('get '+key)
+        const key = req.query
+        console.log('get '+key)
         return res.status(201).json(key)
     },
     async posttxHooks(req,res){
-        const key = req.params.key
-        console.timeLog('post '+key)
+        const key = req.query
+        console.log('post '+key)
         return res.status(201).json(key)
     },
 
--- server/src/routes/instaHookRoutes.js
+++ server/src/routes/instaHookRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = express.Router()
 const instaHooks = require('../controllers/instaHooksController')
 
-routes.get('/gethook/:key',instaHooks.gettxHooks)
-routes.post('/gethook/:key',instaHooks.posttxHooks)
+routes.get('/gethook',instaHooks.gettxHooks)
+routes.post('/gethook',instaHooks.posttxHooks)
 
 module.exports = routes 
